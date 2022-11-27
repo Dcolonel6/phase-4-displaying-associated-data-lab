e7d5661498cdda6774f3af521b6d16233e368352
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-11-28 01:13:54 +0300

    finished assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+
+    def index
+        render json: Item.all, except: [:user_id], include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_user_not_found
+
+    def show
+        user = find_user
+        render json: user, include: :items
+    end
+
+    private
+    def find_user
+        User.find(params[:id])
+    end
+
+    def render_user_not_found
+        render json: {error: "User not Found"}, status: :not_found 
+    end
 end
